5e372bfbc6d08a8192fd2c7a614e518d4be0148d
Nikita Moshyn <m.nikitozzz@gmail.com>
2022-11-10 22:59:15 +0200

    Added a swipe that removes the last character

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet var swipe: UISwipeGestureRecognizer!
     @IBOutlet weak var equalButtonOutlet: UIButton!
     @IBOutlet weak var plusButtonOutlet: UIButton!
     @IBOutlet weak var substractButtonOutlet: UIButton!
@@ -27,18 +28,44 @@ class ViewController: UIViewController {
     
     var numberFromScreen: Double = 0
     var firstNum: Double = 0
-    
+    var firstTempNumber: Double = 0
+    var secondTempNumber: Double = 0
+    let hundred: Double = 100
     
     @IBOutlet weak var resultLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        otherSet()
+        swipeSet()
+    }
+    
+    func otherSet() {
         view.backgroundColor = .black
         resultLabel.text = "\(startNumber)"
         viewHolder.backgroundColor = .black
+    }
+    
+    
+    func swipeSet() {
+        swipe = UISwipeGestureRecognizer(target: self, action: #selector(userSwiped))
+        swipe.direction = .left
+        view.addGestureRecognizer(swipe)
+    }
+    
+    
+    
+    @objc func userSwiped(_ gesture:UISwipeGestureRecognizer) {
+        
+        if var text = resultLabel.text, !text.isEmpty {
+            resultLabel.text = String(text.dropLast())
+        
+        }
         
     }
+    
+    
     @IBAction func signChange(_ sender: UIButton) {
         guard let result = resultLabel.text,
               let result2 = Double(result) else { return }
@@ -55,17 +82,23 @@ class ViewController: UIViewController {
         resultNumber = 0
     }
     
+    
+    
     @IBAction func numberPressed(_ sender: UIButton) {
         guard sender.tag != 10 else {
             if option == true {
                 resultLabel.text = String(sender.tag)
                 option = false
+               
             } else if !resultLabel.text!.contains(".") {
                 resultLabel.text = resultLabel.text! + "."
+               
             }
             resultNumber = Double(resultLabel.text!) ?? 0
             return
         }
+        
+        
         if option == true {
             if resultLabel.text == "0" {
                 resultLabel.text = ""
@@ -78,15 +111,32 @@ class ViewController: UIViewController {
             }
             resultLabel.text = resultLabel.text! + String(sender.tag)
         }
+        
+        
         resultNumber = Double(resultLabel.text!) ?? 0
+        
+        guard sender.tag != 13 else {
+            print("resultNumber - \(secondTempNumber), firstTempNumber - \(firstTempNumber), hundred -\(hundred) ")
+            resultLabel.text = String(format: "%g", resultNumber)
+            return
+        }
+
     }
+    
+    
     @IBAction func calculationButton(_ sender: UIButton) {
         
         
         
+        if let text2 = resultLabel.text, !text2.isEmpty, sender.tag == 13 {
+            
+        }
+        
+        
         if let text = resultLabel.text, !text.isEmpty, sender.tag != 18 {
             firstNum = Double(text) ?? 0
-            
+            firstTempNumber = firstNum
+            print(firstTempNumber)
             switch sender.tag {
             case 14:
                 resultLabel.text = "/"
@@ -96,9 +146,13 @@ class ViewController: UIViewController {
                 resultLabel.text = "-"
             case 17:
                 resultLabel.text = "+"
+//            case 13:
+ //               secondTempNumber = firstNum * resultNumber / hundred
+ //               resultNumber = secondTempNumber
             default:
                 print("Error (calculationButton)")
             }
+
             operation = sender.tag
             option = true
         } else if sender.tag == 18 {
@@ -106,15 +160,19 @@ class ViewController: UIViewController {
             var result = 0.0
             
             
-            if operation == 17 {
+            switch operation {
+            case 17:
                 result = firstNum + resultNumber
-            } else if operation == 16 {
+            case 16:
                 result = firstNum - resultNumber
-            } else if operation == 15 {
+            case 15:
                 result = firstNum * resultNumber
-            } else if operation == 14 {
+            case 14:
                 result = firstNum / resultNumber
+            default:
+                print("Error (calculation)")
             }
+
             resultLabel.text = String(format: "%g", result)
         }
         
